e04743247c09b2277fff69748b33400d8fd97825
Diego Lombardi <diegolombardi199@gmail.com>
2023-11-16 21:39:59 +0100

    esercizio somma

---
+++ sommaese16nov.js
@@ -0,0 +1,18 @@
+let a = 1;
+let b = 1;
+let somma = 0;
+
+
+for( let i = 1; i <= 200; i += 1){
+    
+    somma = somma + i;
+}
+
+console.log(somma)
+
+
+for (let j = 1;j <= 400;j++){
+    if(j%20 == 0){
+        console.log(j);
+    }
+}
